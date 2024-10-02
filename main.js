d37a59649139433fb30de0ce6eba372a4791c10a
Beatriz Arevalo <biaarevalofreitas@gmail.com>
2024-10-02 20:55:08 -0300

    add server

---
+++ main.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const rotas = require("./routers/router.js" );
+
+
+app.use(bodyParser.json());
+
+app.use("/", rotas);
+
+app.listen(8000);
+
+module.exports = app
